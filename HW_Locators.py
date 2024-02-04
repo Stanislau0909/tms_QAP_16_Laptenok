c2850d79b483d45e5ad4612a0f3f972204bcaec6
Stanislav <stanislavlaptenok@gmail.com>
2024-02-05 00:48:33 +0300

    lesson 18 locators , HW there is in file HW_Locators

---
+++ HW_Locators.py
@@ -0,0 +1,44 @@
+import pytest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+# def get_by_data_id(data_id):
+#     return f"//*[data-id={data_id}]"
+#
+# def get_by_class_css(selector):
+#     return f'[class={selector}]         примеры методов для себя
+#
+# def get_locator_by_xpath(driver, selector):
+#     return driver.find_elements(By.CSS_SELECTOR, f"{}")
+
+def test_locators(driver):
+    driver.get("https://www.bbc.com/")
+    logo = (driver.find_element(By.XPATH, "//div[@class='orb-nav-section orb-nav-blocks']")) # 1 element
+    assert logo
+    sign_in = (driver.find_element(By.XPATH, "//span[@id='idcta-username']"))                # 2 element
+    assert sign_in
+    text = "Home"                                                                            # 3 element
+    assert driver.find_element(By.LINK_TEXT, text)
+    text = "Sport"                                                                           # 4 element
+    assert driver.find_element(By.LINK_TEXT, text)
+    text = "Reel"                                                                            # 5 element
+    assert driver.find_element(By.LINK_TEXT, text)
+    text = "Travel"                                                                          # 6 element
+    assert driver.find_element(By.LINK_TEXT, text)
+    class_name = "orbit-header-more"                                                         # 7 element
+    assert driver.find_element(By.CLASS_NAME, class_name)
+    banner = driver.find_element(By.XPATH , "(//div[@class='responsive-image'])[1]")         # 8 element
+    assert banner
+    text_banner = driver.find_element(By.XPATH,"(//a[@class='media__link'])[8]")             # 9 element
+    assert text_banner
+    block_news = "a.module__title__link.tag.tag--news"                                       # 10 element
+    assert driver.find_element(By.CSS_SELECTOR, block_news)
+
+
+
+
--- example_for_GH.py
+++
@@ -1,5 +0,0 @@
-def get_by_data_id(data_id):
-    return f"//*[data-id={data_id}]"
-
-def get_by_class_css(selector):
-    return f'[class={selector}]
